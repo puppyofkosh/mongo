7b52c995be57ee94883c6ace2394d045ede4fac1
Ian Boros <ian.boros@10gen.com>
2017-12-13 10:10:16 -0500

    Some refactoring and linting

--- jstests/aggregation/explain-limit.js
+++ jstests/aggregation/explain-limit.js
@@ -7,38 +7,42 @@
 
     let coll = db.explain;
 
+    const COLLSIZE = 10;
+    const LIMIT = 3;
+
+    function checkResults(results, verbosity) {
+        var cursorSubdocs = getAggPipelineCursorStage(results);
+        for (let elem in cursorSubdocs) {
+            let result = cursorSubdocs[elem];
+            assert.eq(result.limit, NumberLong(LIMIT), tojson(results));
+
+            if (verbosity === "queryPlanner") {
+                assert(!result.hasOwnProperty("executionStats"), tojson(results));
+            } else {
+                // if its "executionStats" or "allPlansExecution"
+                assert.eq(result.executionStats.nReturned, LIMIT, tojson(results));
+                assert.lt(result.executionStats.totalKeysExamined, COLLSIZE, tojson(results));
+                assert.lt(result.executionStats.totalDocsExamined, COLLSIZE, tojson(results));
+            }
+        }
+    }
+
     // Explain() should respect limit.
     coll.drop();
     assert.commandWorked(coll.createIndex({a: 1}));
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < COLLSIZE; i++) {
         assert.writeOK(coll.insert({a: 1}));
     }
 
-    const res1 = db.explain.explain("queryPlanner").aggregate([{$match: {a: 1}}, {$limit: 3}]);
-    getResults(res1, "queryPlanner");
+    const res1 = db.explain.explain("queryPlanner").aggregate([{$match: {a: 1}}, {$limit: LIMIT}]);
+    checkResults(res1, "queryPlanner");
 
-    const res2 = db.explain.explain("executionStats").aggregate([{$match: {a: 1}}, {$limit: 3}]);
-    getResults(res2, "executionStats");
+    const res2 =
+        db.explain.explain("executionStats").aggregate([{$match: {a: 1}}, {$limit: LIMIT}]);
+    checkResults(res2, "executionStats");
 
-    const res3 = db.explain.explain("allPlansExecution").aggregate([{$match: {a: 1}}, {$limit: 3}]);
-    getResults(res3, "allPlansExecution");
-
+    const res3 =
+        db.explain.explain("allPlansExecution").aggregate([{$match: {a: 1}}, {$limit: LIMIT}]);
+    checkResults(res3, "allPlansExecution");
 })();
-
-function getResults(results, verbosity) {
-    for (let elem in getAggPipelineCursorStage(results)) {
-        // TODO: FIXME: don't call this twice
-        let result = getAggPipelineCursorStage(results)[elem];
-        assert.eq(result.limit, NumberLong(3), tojson(results));
-
-        if (verbosity === "queryPlanner") {
-            assert(!result.hasOwnProperty("executionStats"), tojson(results));
-        } else {
-            // if its "executionStats" or "allPlansExecution"
-            assert.eq(result.executionStats.nReturned, 3, tojson(results));
-            assert.lt(result.executionStats.totalKeysExamined, 10, tojson(results));
-            assert.lt(result.executionStats.totalDocsExamined, 10, tojson(results));
-        }
-    }
-}
