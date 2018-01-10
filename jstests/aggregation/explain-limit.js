21c9e47fde3973f8de4146861f1a76957b041bfe
Ian Boros <ian.boros@10gen.com>
2018-01-10 17:42:47 -0500

    Improvements to test

--- jstests/aggregation/explain-limit.js
+++ jstests/aggregation/explain-limit.js
@@ -3,14 +3,15 @@
 (function() {
     "use strict";
 
-    load('jstests/libs/analyze_plan.js');  // For getAggPipelineCursorStage().
+    load("jstests/libs/analyze_plan.js");  // For getAggPipelineCursorStage().
 
-    let coll = db.explain;
+    let coll = db.explain_limit;
 
-    const COLLSIZE = 10;
-    const LIMIT = 3;
+    const MULTIPLANNER_LIMIT = 101;
+    const COLLSIZE = MULTIPLANNER_LIMIT + 5;
+    const LIMIT = 10;
 
-    function checkResults(results, verbosity) {
+    function checkResults(results, verbosity, multiPlanner) {
         var cursorSubdocs = getAggPipelineCursorStage(results);
         for (let elem in cursorSubdocs) {
             let result = cursorSubdocs[elem];
@@ -19,15 +20,25 @@
             if (verbosity === "queryPlanner") {
                 assert(!result.hasOwnProperty("executionStats"), tojson(results));
             } else {
-                // if its "executionStats" or "allPlansExecution"
-                assert.eq(result.executionStats.nReturned, LIMIT, tojson(results));
-                assert.lt(result.executionStats.totalKeysExamined, COLLSIZE, tojson(results));
-                assert.lt(result.executionStats.totalDocsExamined, COLLSIZE, tojson(results));
+                // if it's "executionStats" or "allPlansExecution".
+                if (multiPlanner) {
+                    assert.lte(result.executionStats.nReturned, MULTIPLANNER_LIMIT,
+                               tojson(results));
+                    assert.lte(result.executionStats.totalKeysExamined, MULTIPLANNER_LIMIT,
+                               tojson(results));
+                    assert.lte(result.executionStats.totalDocsExamined, MULTIPLANNER_LIMIT,
+                               tojson(results));
+                } else {
+                    assert.eq(result.executionStats.nReturned, LIMIT, tojson(results));
+                    assert.eq(result.executionStats.totalKeysExamined, LIMIT, tojson(results));
+                    assert.eq(result.executionStats.totalDocsExamined, LIMIT, tojson(results));
+                }
+
             }
         }
     }
 
-    // Explain() should respect limit.
+    // explain() should respect limit.
     coll.drop();
     assert.commandWorked(coll.createIndex({a: 1}));
 
@@ -35,14 +46,26 @@
         assert.writeOK(coll.insert({a: 1}));
     }
 
-    const res1 = db.explain.explain("queryPlanner").aggregate([{$match: {a: 1}}, {$limit: LIMIT}]);
-    checkResults(res1, "queryPlanner");
+    const pipeline = [{$match: {a: 1}}, {$limit: LIMIT}];
 
-    const res2 =
-        db.explain.explain("executionStats").aggregate([{$match: {a: 1}}, {$limit: LIMIT}]);
-    checkResults(res2, "executionStats");
+    const res1 = coll.explain("queryPlanner").aggregate(pipeline);
+    checkResults(res1, "queryPlanner", false);
 
-    const res3 =
-        db.explain.explain("allPlansExecution").aggregate([{$match: {a: 1}}, {$limit: LIMIT}]);
-    checkResults(res3, "allPlansExecution");
+    const res2 = coll.explain("executionStats").aggregate(pipeline);
+    checkResults(res2, "executionStats", false);
+
+    const res3 = coll.explain("allPlansExecution").aggregate(pipeline);
+    checkResults(res3, "allPlansExecution", false);
+
+    // Create a second index so that we're forced to use the MultiPlanner.
+    assert.commandWorked(coll.createIndex({a: 1, b: 1}));
+
+    const res4 = coll.explain("queryPlanner").aggregate(pipeline);
+    checkResults(res4, "queryPlanner", true);
+
+    const res5 = coll.explain("executionStats").aggregate(pipeline);
+    checkResults(res5, "executionStats", true);
+
+    const res6 = coll.explain("allPlansExecution").aggregate(pipeline);
+    checkResults(res6, "allPlansExecution", true);
 })();
