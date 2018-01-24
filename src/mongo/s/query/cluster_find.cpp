23ffce4cb7a0de289172e925f0f78578372ecb8d
Ian Boros <ian.boros@10gen.com>
2018-01-24 12:29:54 -0500

    Fix case for change streams

--- src/mongo/s/query/cluster_find.cpp
+++ src/mongo/s/query/cluster_find.cpp
@@ -466,7 +466,14 @@ StatusWith<CursorResponse> ClusterFind::runGetMore(OperationContext* opCtx,
 
         const auto interruptStatus = opCtx->checkForInterruptNoAssert();
         if (!interruptStatus.isOK()) {
-            return interruptStatus;
+            if (pinnedCursor.getValue().isTailableAndAwaitData() &&
+                interruptStatus == ErrorCodes::ExceededTimeLimit) {
+                // Whatever. We should return an empty batch.
+                // This was likely caused by the deadline we set earlier in this function.
+            } else {
+                // We actually got interrupted by some other event.
+                return interruptStatus;
+            }
         }
 
         if (next.getValue().isEOF()) {
