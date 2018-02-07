68d2b0dfdac8506df9559ad0b3c77dc66c1328ee
Ian Boros <ian.boros@10gen.com>
2018-02-07 15:31:29 -0500

    Revert "Start work on test"
    This reverts commit 8d12d31e550909076d08d21693f7be844aab1796.

--- jstests/sharding/kill_pinned_cursor.js
+++ jstests/sharding/kill_pinned_cursor.js
@@ -9,6 +9,8 @@
 (function() {
     "use strict";
 
+    const kFailPointName = "keepCursorPinnedDuringGetMore";
+
     const st = new ShardingTest({shards: 2});
     const kDBName = "test";
     const mongosDB = st.s.getDB(kDBName);
@@ -23,89 +25,70 @@
 
     st.shardColl(coll, {_id: 1}, {_id: 5}, {_id: 6}, kDBName, false);
 
-    function runPinnedCursorKillTest({failPointName, runGetMoreFunc}) {
-        // Set up the first mongod to hang on a getMore request.
-        let cleanup = null;
-        let cursorId;
-        
-        try {
-            // ONLY set the failpoint on the first mongod. Setting the failpoint on the mongos will
-            // only cause it to spin, and not actually send any requests out.
-            assert.commandWorked(
-                shard0DB.adminCommand({configureFailPoint: failPointName, mode: "alwaysOn"}));
+    // Set up the first mongod to hang on a getMore request.
+    let cleanup = null;
+    let cursorId;
 
-            // Run a find with a sort, so that we must return the results from shard 0 before the
-            // results from shard 1. This should cause the mongos to hang, waiting on the network
-            // for the response from the hung shard.
-            let cmdRes = mongosDB.runCommand({find: coll.getName(), sort: {_id: 1}, batchSize: 2});
-            assert.commandWorked(cmdRes);
-            cursorId = cmdRes.cursor.id;
-            assert.neq(cursorId, NumberLong(0));
+    try {
+        // ONLY set the failpoint on the first mongod. Setting the failpoint on the mongos will
+        // only cause it to spin, and not actually send any requests out.
+        assert.commandWorked(
+            shard0DB.adminCommand({configureFailPoint: kFailPointName, mode: "alwaysOn"}));
 
-            let code = `let cursorId = ${cursorId.toString()};`;
-            code += `let collName = "${coll.getName()}";`;
-            code += `(${runGetMoreFunc.toString()})();`;
-            cleanup = startParallelShell(code, st.s.port);
+        // Run a find with a sort, so that we must return the results from shard 0 before the
+        // results from shard 1. This should cause the mongos to hang, waiting on the network for
+        // the response from the hung shard.
+        let cmdRes = mongosDB.runCommand({find: coll.getName(), sort: {_id: 1}, batchSize: 2});
+        assert.commandWorked(cmdRes);
+        cursorId = cmdRes.cursor.id;
+        assert.neq(cursorId, NumberLong(0));
 
-            assert.soon(() => shard0DB.serverStatus().metrics.cursor.open.pinned > 0);
-
-            cmdRes = mongosDB.runCommand({killCursors: coll.getName(), cursors: [cursorId]});
-
-            // Now that we know the cursor is stuck waiting on a remote to respond, we test that we
-            // can still successfully kill it without hanging or returning a "CursorInUse" error.
-            assert.commandWorked(cmdRes);
-            assert.eq(cmdRes.cursorsKilled, [cursorId]);
-            assert.eq(cmdRes.cursorsAlive, []);
-            assert.eq(cmdRes.cursorsNotFound, []);
-            assert.eq(cmdRes.cursorsUnknown, []);
-        } finally {
-            assert.commandWorked(
-                shard0DB.adminCommand({configureFailPoint: failPointName, mode: "off"}));
-            if (cleanup) {
-                cleanup();
-            }
-        }
-
-        // Eventually the cursor on the mongod should be cleaned up, now that we've disabled the
-        // failpoint.
-        assert.soon(() => shard0DB.serverStatus().metrics.cursor.open.pinned == 0);
-
-        // Eventually the cursor should get reaped, at which point the next call to killCursors
-        // should report that nothing was killed.
-        let cmdRes = null;
-        assert.soon(function() {
-            cmdRes = mongosDB.runCommand({killCursors: coll.getName(), cursors: [cursorId]});
-            assert.commandWorked(cmdRes);
-            return cmdRes.cursorsKilled.length == 0;
-        });
-
-        assert.eq(cmdRes.cursorsAlive, []);
-        assert.eq(cmdRes.cursorsNotFound, [cursorId]);
-        assert.eq(cmdRes.cursorsUnknown, []);
-    }
-
-    // Test that killing the pinned cursor before it starts building the batch results in a
-    // CursorKilled exception.
-    let testParameters = {
-        failPointName: "keepCursorPinnedDuringGetMore",
-        runGetMoreFunc: function() {
-            const response = db.runCommand({getMore: cursorId, collection: collName});
+        const runGetMore = function() {
+            let response = db.runCommand({getMore: cursorId, collection: collName});
             // We expect that the operation will get interrupted and fail.
             assert.commandFailedWithCode(response, ErrorCodes.CursorKilled);
-        }
-    };
-    runPinnedCursorKillTest(testParameters);
+        };
+        let code = `let cursorId = ${cursorId.toString()};`;
+        code += `let collName = "${coll.getName()}";`;
+        code += `(${runGetMore.toString()})();`;
+        cleanup = startParallelShell(code, st.s.port);
 
-    // // Test that, if the pinned cursor is killed after it has finished building a batch, that batch
-    // // is returned to the client but a subsequent getMore will fail with a 'CursorKilled' exception.
-    // testParameters.failPointName = "waitBeforeUnpinningCursorAfterGetMoreBatch";
-    // testParameters.runGetMoreFunc = function() {
-    //     const getMoreCmd = {getMore: cursorId, collection: collName, batchSize: 2};
-    //     // We expect that the first getMore will succeed, while the second fails because the cursor
-    //     // has been killed.
-    //     assert.commandWorked(db.runCommand(getMoreCmd));
-    //     assert.commandFailedWithCode(db.runCommand(getMoreCmd), ErrorCodes.CursorKilled);
-    // };
+        // Sleep until we know the cursor is pinned on the mongod.
+        assert.soon(() => shard0DB.serverStatus().metrics.cursor.open.pinned > 0);
+
+        cmdRes = mongosDB.runCommand({killCursors: coll.getName(), cursors: [cursorId]});
+
+        // Now that we know the cursor is stuck waiting on a remote to respond, we test that we can
+        // still successfully kill it without hanging or returning a "CursorInUse" error.
+        assert.commandWorked(cmdRes);
+        assert.eq(cmdRes.cursorsKilled, [cursorId]);
+        assert.eq(cmdRes.cursorsAlive, []);
+        assert.eq(cmdRes.cursorsNotFound, []);
+        assert.eq(cmdRes.cursorsUnknown, []);
+    } finally {
+        assert.commandWorked(
+            shard0DB.adminCommand({configureFailPoint: kFailPointName, mode: "off"}));
+        if (cleanup) {
+            cleanup();
+        }
+    }
+
+    // Eventually the cursor on the mongod should be cleaned up, now that we've disabled the
+    // failpoint.
+    assert.soon(() => shard0DB.serverStatus().metrics.cursor.open.pinned == 0);
+
+    // Eventually the cursor should get reaped, at which point the next call to killCursors
+    // should report that nothing was killed.
+    let cmdRes = null;
+    assert.soon(function() {
+        cmdRes = mongosDB.runCommand({killCursors: coll.getName(), cursors: [cursorId]});
+        assert.commandWorked(cmdRes);
+        return cmdRes.cursorsKilled.length == 0;
+    });
+
+    assert.eq(cmdRes.cursorsAlive, []);
+    assert.eq(cmdRes.cursorsNotFound, [cursorId]);
+    assert.eq(cmdRes.cursorsUnknown, []);
 
     st.stop();
 })();
