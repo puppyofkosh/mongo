38170cd1166dee883ebd62863dd7a3b70143eeea
Ian Boros <ian.boros@10gen.com>
2018-03-08 12:56:02 -0500

    comment

--- jstests/sharding/accurate_count_with_predicate.js
+++ jstests/sharding/accurate_count_with_predicate.js
@@ -2,11 +2,9 @@
  * Tests that Collection.count(), when run with a predicate (not a "fast count"), filters out
  * orphan documents. This is intended to test the fix for SERVER-3645.
  *
- * The test works by inserting a bunch of documents, enabling the balancer to trigger a moveChunk,
- * and while the moveChunk is happening, running count().
- *
- * Resource intensive because it writes 10000 documents.
- * @tags: [resource_intensive]
+ * The test works by sharding a collection, and then inserting orphan documents directly into one
+ * of the shards. It then runs a count() and ensures that the orphan documents are not counted
+ * twice.
  */
 (function() {
     "use strict";
@@ -16,20 +14,18 @@
     const kNs = "test.slowcount";
     const shard0Coll = st.shard0.getCollection(kNs);
     const admin = st.getDB("admin");
-    const num = 10000;
+    const num = 10;
     const middle = num / 2;
-
-    assert.commandWorked(admin.runCommand({enableSharding: "test"}));
-    assert.commandWorked(admin.runCommand({movePrimary: "test", to: shards[0]._id}));
-    assert.commandWorked(admin.runCommand({shardCollection: kNs, key: {x: 1}}));
-
-    assert.commandWorked(admin.runCommand({split: kNs, middle: {x: middle}}));
-    assert.commandWorked(admin.runCommand(
-        {moveChunk: kNs, find: {x: middle}, to: shards[1]._id, _waitForDelete: true}));
 
     function getNthDocument(n) {
         return {_id: n, one: 1, x: n};
     }
+
+    // Shard the collection. Shard 0 will get keys from [0, middle) and shard 1 will get everything
+    // from [middle, num).
+    assert.commandWorked(admin.runCommand({enableSharding: "test"}));
+    st.ensurePrimaryShard("test", st.shard0.name);
+    st.shardColl("slowcount", {x: 1}, {x: middle}, {x: middle + 1}, "test", true);
 
     // Insert some docs.
     for (let i = 0; i < num; i++) {
@@ -38,7 +34,7 @@
 
     // Insert some orphan documents to shard 0. These are just documents outside the range
     // which shard 0 owns.
-    for (let i = middle + 1; i < middle + 11; i++) {
+    for (let i = middle + 1; i < middle + 3; i++) {
         assert.writeOK(shard0Coll.insert(getNthDocument(i)));
     }
 
