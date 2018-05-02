45cdae8549267b6e6cb9c204e8af35d5927bb636
Ian Boros <ian.boros@10gen.com>
2018-05-02 13:12:53 -0400

    more inserts

--- jstests/sharding/change_stream_resume_from_different_mongos.js
+++ jstests/sharding/change_stream_resume_from_different_mongos.js
@@ -37,14 +37,16 @@
         const s1DB = st.s1.getDB("test");
         const coll = assertDropAndRecreateCollection(s0DB, "change_stream_failover");
 
+        const nDocs = 100;
+
         // TODO: Use moar docs!~!!
         // Split so ids < 5 are for one shard, ids >= 5 for another.
         st.shardColl(coll,
-                     {_id: 1},  // key
-                     {_id: 5},  // split
-                     {_id: 6},  // move
-                     "test",    // dbName
-                     false      // waitForDelete
+                     {_id: 1},          // key
+                     {_id: nDocs},      // split
+                     {_id: nDocs + 1},  // move
+                     "test",            // dbName
+                     false              // waitForDelete
                      );
 
         // Open a change stream.
@@ -54,15 +56,21 @@
         // Be sure we can read from the change stream. Write some documents that will end up on
         // each shard. Use a bulk write to increase the chance that two of the writes get the same
         // cluster time on each shard.
-        const kIdsToInsert = [0, 7, 2];
-        
+        const kIdsToInsert = [];
+        for (let i = 0; i < nDocs / 2; i++) {
+            // Interleave elements which will end up on shard 0 with elements that will end up on
+            // shard 1.
+            kIdsToInsert.push(i);
+            kIdsToInsert.push(i + nDocs);
+        }
+
         assert.writeOK(coll.insert(kIdsToInsert.map(objId => {return {_id: objId}})));
 
         // Read from the change stream. The order of the documents isn't guaranteed because we
         // performed a bulk write.
         const firstChange = cst.getOneChange(changeStream);
         const docsFoundInOrder = [firstChange];
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < nDocs - 1; i++) {
             const change = cst.getOneChange(changeStream);
             assert.docEq(change.ns, {db: s0DB.getName(), coll: coll.getName()});
             assert.eq(change.operationType, "insert");
@@ -73,7 +81,7 @@
         // Assert that we found the documents we inserted (in any order).
         assert(setEq(new Set(kIdsToInsert),
                      new Set(docsFoundInOrder.map(doc => doc.fullDocument._id))));
-        
+
         // Now resume using the resume token from the first change on a different mongos.
         const otherCst =
             new ChangeStreamTest(ChangeStreamTest.getDBForChangeStream(watchMode, s1DB));
@@ -83,8 +91,8 @@
 
         // Be sure we can read the remaining changes, in the same order as they were read on the
         // first stream.
-        otherCst.assertNextChangesEqual({cursor: resumeCursor,
-                                         expectedChanges: docsFoundInOrder.splice(1)});
+        otherCst.assertNextChangesEqual(
+            {cursor: resumeCursor, expectedChanges: docsFoundInOrder.splice(1)});
     }
 
     st.stop();
