c5e341bf7992fee049a35c01ce4cf4a89cd31637
Ian Boros <ian.boros@10gen.com>
2018-05-02 11:57:20 -0400

    test improvement

--- jstests/noPassthrough/group_interrupt_js_execution.js
+++ jstests/noPassthrough/group_interrupt_js_execution.js
@@ -1,29 +1,26 @@
-// TODO: move to noPassthrough
+// Test what happens when javascript execution inside the group command is interrupted, either from
+// killOp, or due to timeout.
 (function() {
-    var replTest = new ReplSetTest({name: 'testSet', nodes: 2});
-    var nodes = replTest.startSet();
-    replTest.initiate();
-    var master = replTest.getPrimary();
+    const conn = MongoRunner.runMongod({});
+    assert.neq(null, conn);
 
-    const db = master.getDB("test");
+    const db = conn.getDB("test");
     const coll = db.group_with_stepdown;
-    
-    coll.insert({name: 'bob', foo: 1});
-    coll.insert({name: 'bob', foo: 2});
-    coll.insert({name: 'alice', foo: 1});
-    coll.insert({name: 'alice', foo: 3});
-    coll.insert({name: 'fred', foo: 3});
-    coll.insert({name: 'fred', foo: 4});
 
+    coll.insert({name: "bob", foo: 1});
+    coll.insert({name: "alice", foo: 1});
+    coll.insert({name: "fred", foo: 3});
+    coll.insert({name: "fred", foo: 4});
 
+    // Attempts to run the group command while the given failpoint is enabled. If 'shouldKillOp' is
+    // true, it will run killOp on the operation while it is hanging. If 'shouldKillOp' is false,
+    // it will let the operation hang until the javascript execution timeout is reached.
     function runTest(failPointName, shouldKillOp) {
         jsTestLog("Running with failPoint: " + failPointName + "shouldKillOp: " + shouldKillOp);
         let awaitShellFn = null;
         try {
             assert.commandWorked(
-                db.adminCommand({configureFailPoint: failPointName,
-                                 mode: "alwaysOn"}));
-            print("ian: 1");
+                db.adminCommand({configureFailPoint: failPointName, mode: "alwaysOn"}));
 
             // Run a group in the background. Wait until we hit the failpoint.
             function runHangingGroup() {
@@ -31,29 +28,25 @@
 
                 const err = assert.throws(() => coll.group({
                     key: {foo: 1},
-                    initial: {count: 0, values: []},
+                    initial: {count: 0},
                     reduce: function(obj, prev) {
                         prev.count++;
-                        prev.values.push(obj.name);
                     }
-                }), [], "expected group() to fail");
+                }),
+                                          [],
+                                          "expected group() to fail");
 
-                print("ian: the error is " + tojson(err));
                 assert.eq(err.code, ErrorCodes.Interrupted);
             }
-            awaitShellFn = startParallelShell(runHangingGroup, master.port);
-            print("ian: 2");
+            awaitShellFn = startParallelShell(runHangingGroup, conn.port);
 
             // Wait until we know the failpoint has been reached.
             let opid = null;
             assert.soon(function() {
-                const arr =
-                      db.getSiblingDB("admin")
-                      .aggregate(
-                          [{$currentOp: {}}, {$match: {"msg": failPointName}}])
-                      .toArray();
+                const arr = db.getSiblingDB("admin")
+                                .aggregate([{$currentOp: {}}, {$match: {"msg": failPointName}}])
+                                .toArray();
 
-                print("array return is " + tojson(arr));
                 if (arr.length == 0) {
                     return false;
                 }
@@ -66,28 +59,19 @@
             assert.neq(opid, null);
 
             if (shouldKillOp) {
-                print("ian: 3");
-
                 // Kill the op running group.
                 assert.commandWorked(db.killOp(opid));
             } else {
                 // The javascript execution should time out on its own eventually, even if we don't
                 // run killOp.
             }
-
-            print("ian: 4");
         } finally {
-            assert.commandWorked(db.adminCommand({configureFailPoint: failPointName,
-                                                  mode: "off"}));
-
-            print("ian: 5");
+            assert.commandWorked(db.adminCommand({configureFailPoint: failPointName, mode: "off"}));
 
             if (awaitShellFn) {
                 awaitShellFn();
             }
         }
-
-        print("ian: 6");
     }
 
     const kFailPoints = ["hangInGroupJsReduceInit", "hangInGroupJsCleanup"];
@@ -97,5 +81,5 @@
         }
     }
 
-    replTest.stopSet();
+    assert.eq(0, MongoRunner.stopMongod(conn), "expected mongod to shutdown cleanly");
 })();
