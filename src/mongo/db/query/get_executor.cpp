b61814a372b42e2b8f3ade021d4b9490e9b260f5
Ian Boros <ian.boros@10gen.com>
2018-06-06 12:00:34 -0400

    charlie feedback part 2

--- src/mongo/db/query/get_executor.cpp
+++ src/mongo/db/query/get_executor.cpp
@@ -359,8 +359,7 @@ StatusWith<PrepareExecutionResult> prepareExecution(OperationContext* opCtx,
     }
 
     // Try to look up a cached solution for the query.
-    auto cs = collection->infoCache()->getPlanCache()->decideShouldUseCache(*canonicalQuery);
-    if (cs) {
+    if (auto cs = collection->infoCache()->getPlanCache()->decideShouldUseCache(*canonicalQuery)) {
         // We have a CachedSolution.  Have the planner turn it into a QuerySolution.
         auto statusWithQs = QueryPlanner::planFromCache(*canonicalQuery, plannerParams, *cs);
 
