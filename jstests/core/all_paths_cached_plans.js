824e9f6cc60017b4795171262910329d576a511f
Ian Boros <ian.boros@10gen.com>
2018-08-13 15:26:03 -0400

    flesh out test

--- jstests/core/all_paths_cached_plans.js
+++ jstests/core/all_paths_cached_plans.js
@@ -1,8 +1,11 @@
 /**
  * Test that cached plans which use allPaths indexes work.
- * TODO: Flesh out!
  */
 (function() {
+    "use strict";
+
+    load('jstests/libs/analyze_plan.js');  // For getPlanStage().
+
     assert.commandWorked(
         db.adminCommand({setParameter: 1, internalQueryAllowAllPathsIndexes: true}));
 
@@ -10,6 +13,10 @@
     coll.drop();
     assert.commandWorked(coll.createIndex({"b.$**": 1}));
     assert.commandWorked(coll.createIndex({"a": 1}));
+
+    // In order for the plan cache to be used, there must be more than one plan available.  Insert
+    // data into the collection such that the b.$** will be far more selective than the index on
+    // 'a'.
     for (let i = 0; i < 1000; i++) {
         assert.commandWorked(coll.insert({a: 1}));
     }
@@ -28,21 +35,30 @@
     // The plan cache should be empty.
     assert.eq(getPlansForCacheEntry(q).length, 0);
 
-    let res = coll.find(q).toArray();
-    printjson(res);
-    assert.eq(res.length, 1, () => tojson(res));
+    // Run the query twice, so the plan gets cached.
+    for (let i = 0; i < 2; i++) {
+        assert.eq(coll.find(q).itcount(), 1);
+    }
 
-    res = coll.find(q).toArray();
-    assert.eq(res.length, 1, () => tojson(res));
+    // The plan cache should no longer be empty. Check that the chosen plan uses the b.$** index.
+    const plan = getPlansForCacheEntry(q)[0].reason.stats;
+    const ixScanStage = getPlanStage(plan, "IXSCAN");
+    assert.neq(ixScanStage, null, plan);
+    assert.eq(ixScanStage.keyPattern.b, 1, plan);
 
-    // The plan cache should be empty.
-    assert.gt(getPlansForCacheEntry(q).length, 0);
+    // Run the query again. This time it should use the cached plan. We should get the same result
+    // as earlier.
+    assert.eq(coll.find(q).itcount(), 1);
 
-    res = coll.find(q).toArray();
-    assert.eq(res.length, 1, () => tojson(res));
+    // Now run a query where b is null. This should have a different shape key from the previous
+    // query since the index is sparse.
+    for (let i = 0; i < 2; i++) {
+        assert.eq(coll.find({a: 1, b: null}).itcount(), 1000);
+    }
 
-    // Now run a query where a is null. This should have a different shape key from the previous
-    // query.
-    res = coll.find({a: 1, b: null}).toArray();
-    assert.eq(res.length, 1000, () => tojson(res));
+    // There should only have been one solution for the above query, so it would not get cached.
+    assert.eq(getPlansForCacheEntry({a: 1, b: null}).length, 0);
+
+    // TODO SERVER-35326: Update this test to use a $** index with a collation.
+    // TODO SERVER-35336: Update this test to use a partial $** index .
 })();
