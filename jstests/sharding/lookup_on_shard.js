46feb10f4e59c8293edd5447e5a9a721cbef9bd9
Ian Boros <ian.boros@10gen.com>
2019-01-09 16:50:41 -0500

    Got test working with both forms of sharding

--- jstests/sharding/lookup_on_shard.js
+++ jstests/sharding/lookup_on_shard.js
@@ -7,24 +7,6 @@
 
     // TODO: Rewrite test in a way such that it can run with the mainColl sharded and unsharded.
 
-    // Shard the main collection.
-    sharded.shardColl("mainColl",
-                      {_id: 1},  // shard key
-                      {_id: 5},  // split
-                      {_id: 5},  // move
-                      "test",    // dbName
-                      true       // waitForDelete
-                      );
-
-    // Shard the foreign collection.
-    sharded.shardColl("foreignColl",
-                      {_id: 1},  // shard key
-                      {_id: 5},  // split
-                      {_id: 5},  // move
-                      "test",    // dbName
-                      true       // waitForDelete
-                      );
-
     const coll = sharded.s.getDB('test').mainColl;
     const foreignColl = sharded.s.getDB('test').foreignColl;
     const smallColl = sharded.s.getDB("test").smallColl;
@@ -45,65 +27,85 @@
     print("ian: main coll shard distribution: " + tojson(coll.getShardDistribution()));
     print("ian: foreign coll shard distribution: " + tojson(foreignColl.getShardDistribution()));
 
-    (function() {
-        // Run a pipeline which must be merged on a shard. This should force the $lookup (on the
-        // sharded collection) to be run on a mongod.
-        pipeline = [
-            {
-              $lookup: {
-                  localField: "foreignId",
-                  foreignField: "key",
-                  from: "foreignColl",
-                  as: "foreignDoc"
-              }
-            },
-            {$_internalSplitPipeline: {mergeType: "anyShard"}}
-        ];
+    const runTest = function() {
+        (function() {
+            // Run a pipeline which must be merged on a shard. This should force the $lookup (on the
+            // sharded collection) to be run on a mongod.
+            pipeline = [
+                {
+                  $lookup: {
+                      localField: "foreignId",
+                      foreignField: "key",
+                      from: "foreignColl",
+                      as: "foreignDoc"
+                  }
+                },
+                {$_internalSplitPipeline: {mergeType: "anyShard"}}
+            ];
 
-        print("ian: Running aggregation");
-        const results = coll.aggregate(pipeline, {allowDiskUse: true}).toArray();
-        print("ian: " + tojson(results));
-        assert.eq(results.length, nDocsMainColl);
-        for (let i = 0; i < results.length; i++) {
-            assert.eq(results[i].foreignDoc.length, 2, results[i]);
-        }
-    })();
-
-    (function() {
-        // TODO
-        return;
-
-        // Pipeline with $lookup inside $lookup.
-        pipeline = [
-            {
-              $lookup: {
-                  from: "foreignColl",
-                  as: "foreignDoc",
-                  pipeline: [
-                      {$lookup: {from: "smallColl", as: "doc", pipeline: []}},
-                  ],
-              }
-            },
-            {$_internalSplitPipeline: {mergeType: "anyShard"}}
-        ];
-        const results = coll.aggregate(pipeline).toArray();
-        print("ian: res is " + tojson(results));
-
-        const expl = coll.explain().aggregate(pipeline);
-        print("ian: explain is " + tojson(expl));
-
-        assert.eq(results.length, nDocsMainColl);
-        for (let i = 0; i < results.length; i++) {
-            assert.eq(results[i].foreignDoc.length, nDocsForeignColl);
-            for (let j = 0; j < nDocsForeignColl; j++) {
-                // Each document pulled from the foreign collection should have one document from
-                // "smallColl":
-                assert.eq(results[i].foreignDoc[j].collName, "foreignColl");
-                assert.eq(results[i].foreignDoc[j].doc.length, 1);
-                assert.eq(results[i].foreignDoc[j].doc[0].collName, "smallColl");
+            const results = coll.aggregate(pipeline).toArray();
+            assert.eq(results.length, nDocsMainColl);
+            for (let i = 0; i < results.length; i++) {
+                assert.eq(results[i].foreignDoc.length, 2, results[i]);
             }
-        }
-    })();
+        })();
+
+        (function() {
+            // Pipeline with $lookup inside $lookup.
+            pipeline = [
+                {
+                  $lookup: {
+                      from: "foreignColl",
+                      as: "foreignDoc",
+                      pipeline: [
+                          {$lookup: {from: "smallColl", as: "doc", pipeline: []}},
+                      ],
+                  }
+                },
+                {$_internalSplitPipeline: {mergeType: "anyShard"}}
+            ];
+            const results = coll.aggregate(pipeline).toArray();
+            print("ian: res is " + tojson(results));
+
+            assert.eq(results.length, nDocsMainColl);
+            for (let i = 0; i < results.length; i++) {
+                assert.eq(results[i].foreignDoc.length, nDocsForeignColl);
+                for (let j = 0; j < nDocsForeignColl; j++) {
+                    // Each document pulled from the foreign collection should have one document
+                    // from
+                    // "smallColl":
+                    assert.eq(results[i].foreignDoc[j].collName, "foreignColl");
+                    assert.eq(results[i].foreignDoc[j].doc.length, 1);
+                    assert.eq(results[i].foreignDoc[j].doc[0].collName, "smallColl");
+                }
+            }
+        })();
+    };
+
+    // Run test with neither collection sharded.
+
+    jsTestLog("Running test with neither collection sharded");
+    runTest();
+
+    jsTestLog("Running test with foreign collection sharded");
+    sharded.shardColl("foreignColl",
+                      {_id: 1},  // shard key
+                      {_id: 5},  // split
+                      {_id: 5},  // move
+                      "test",    // dbName
+                      true       // waitForDelete
+                      );
+    runTest();
+
+    jsTestLog("Running test with main and foreign collection sharded");
+    sharded.shardColl("mainColl",
+                      {_id: 1},  // shard key
+                      {_id: 5},  // split
+                      {_id: 5},  // move
+                      "test",    // dbName
+                      true       // waitForDelete
+                      );
+    runTest();
 
     sharded.stop();
 })();
