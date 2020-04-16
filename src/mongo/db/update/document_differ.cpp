c16869989db557057dcf4603c07358be7c298578
Ian Boros <ian.boros@mongodb.com>
2020-04-16 19:08:13 -0400

    Forgot to add some files. Also, switched the differ to use a new field data structure

---
+++ src/mongo/db/update/document_differ.cpp
@@ -0,0 +1,138 @@
+/**
+ *    Copyright (C) 2019-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/platform/basic.h"
+
+#include "mongo/db/update/document_differ.h"
+
+namespace mongo {
+namespace doc_diff {
+DocumentDiff DocumentDiff::computeDiffHelper(const BSONObj& pre,
+                                             const BSONObj& post,
+                                             const ArrayIndexPath& prefix) {
+    BSONObjIterator preIt(pre);
+    BSONObjIterator postIt(post);
+
+    DocumentDiff ret;
+
+    while (preIt.more() && postIt.more()) {
+        auto preElt = *preIt;
+        auto postElt = *postIt;
+
+        if (preElt.fieldNameStringData() == postElt.fieldNameStringData()) {
+            if (preElt.binaryEqual(postElt)) {
+                // They're identical. Move on.
+            } else if (preElt.type() == BSONType::Object && postElt.type() == BSONType::Object) {
+                // Both are objects, but not identical. We can compute the diff and merge it.
+                ArrayIndexPath newPrefix(prefix);
+                newPrefix.append(preElt.fieldName());
+                DocumentDiff subDiff =
+                    computeDiffHelper(preElt.embeddedObject(), postElt.embeddedObject(), newPrefix);
+
+                // TODO: compute rough size of the diff. If it is bigger than postElt, we should
+                // record this as a simple "set" of the subfield rather than a diff of it.
+                // TODO: Write a test for this.
+                ret.merge(std::move(subDiff));
+//            } else if (preElt.type() == BSONType::Array && postElt.type() == BSONType::Array) {
+            } else {
+                // Record this as an overwrite.
+                ArrayIndexPath newFr(prefix);
+                newFr.append({postElt.fieldName()});
+                ret._toUpsert.push_back({std::move(newFr), postElt});
+            }
+
+            preIt.next();
+            postIt.next();
+        } else {
+            // Record this as a deletion.
+            ArrayIndexPath remove(prefix);
+            remove.append(preElt.fieldName());
+            ret._toDelete.push_back({std::move(remove)});
+            preIt.next();
+        }
+    }
+
+    // Record remaining fields in preElt as removals.
+    while (preIt.more()) {
+        ArrayIndexPath remove(prefix);
+        remove.append((*preIt).fieldName());
+        ret._toDelete.push_back({std::move(remove)});
+        preIt.next();
+    }
+
+    while (postIt.more()) {
+        // Record these as creates.
+        ArrayIndexPath insertFr(prefix);
+        insertFr.append((*postIt).fieldName());
+        ret._toInsert.push_back({insertFr, (*postIt)});
+        // If a field exists in the insert region, ensure that it doesn't also exist in the remove
+        // region.
+        // TODO: In real version make sure this isn't n^2.
+        for (auto itr = ret._toDelete.cbegin(); itr != ret._toDelete.cend(); itr++) {
+            if (insertFr == (*itr)) {
+                ret._toDelete.erase(itr);
+                break;
+            }
+        }
+        postIt.next();
+    }
+
+    return ret;
+}
+
+DocumentDiff DocumentDiff::computeDiff(const BSONObj& pre, const BSONObj& post) {
+    return computeDiffHelper(pre, post, ArrayIndexPath{});
+}
+
+void DocumentDiff::merge(DocumentDiff&& other) {
+    _toUpsert.insert(_toUpsert.end(), other._toUpsert.begin(), other._toUpsert.end());
+    _toDelete.insert(_toDelete.end(), other._toDelete.begin(), other._toDelete.end());
+    _toInsert.insert(_toInsert.end(), other._toInsert.begin(), other._toInsert.end());
+}
+
+std::string DocumentDiff::toStringDebug() const {
+    std::string ret;
+    for (auto&& r : _toDelete) {
+        ret += "remove: " + r.debugString() + "\n";
+    }
+
+    for (auto&& s : _toUpsert) {
+        ret += str::stream() << "insert: "
+                             << s.first.debugString() << " " << s.second.toString(false) << "\n";
+    }
+
+    for (auto&& s : _toInsert) {
+        ret += str::stream() << "create: " << s.first.debugString()
+                             << " " << s.second.toString(false) << "\n";
+    }
+
+    return ret;
+}
+}  // namespace doc_diff
+};  // namespace mongo
---
+++ src/mongo/db/update/document_differ.h
@@ -0,0 +1,149 @@
+/**
+ *    Copyright (C) 2018-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "mongo/bson/bsonobj.h"
+#include "mongo/db/field_ref.h"
+#include "mongo/stdx/variant.h"
+
+namespace mongo {
+
+namespace doc_diff {
+
+class ArrayIndexPath {
+public:
+    using Component = stdx::variant<size_t, std::string>;
+
+    void append(Component c) {
+        _components.push_back(std::move(c));
+    }
+
+    const std::vector<Component>& components() const {
+        return _components;
+    }
+
+    std::string debugString() const {
+        return toString();
+    }
+
+    std::string toString() const {
+        // TODO: This syntax is fake.
+        std::string s;
+        for (size_t i = 0; i < _components.size(); ++i) {
+            const auto& c = _components[i];
+            if (stdx::holds_alternative<size_t>(c)) {
+                s += "$[" + std::to_string(stdx::get<size_t>(c)) + "]";
+            } else {
+                s += stdx::get<std::string>(c);
+            }
+
+            if (i + 1 != _components.size()) {
+                s += ".";
+            }
+        }
+        return s;
+    }
+
+    size_t approximateSizeInBytes() const {
+        // TODO: Someday we need to do this well, but not today.
+        return debugString().size();
+    }
+
+    bool operator==(const ArrayIndexPath& o) const {
+        if (_components.size() != o._components.size()) {
+            return false;
+        }
+        for (size_t i = 0; i < _components.size(); ++i) {
+            if (_components[i] != o._components[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+private:
+
+    std::vector<Component> _components;
+};
+    
+// The BSONObjs from which this is constructed must outlive the Diff itself.
+class DocumentDiff {
+public:
+    static DocumentDiff computeDiff(const BSONObj& pre, const BSONObj& post);
+
+    void merge(DocumentDiff&& other);
+
+    const std::vector<ArrayIndexPath>& toDelete() const {
+        return _toDelete;
+    }
+
+    const std::vector<std::pair<ArrayIndexPath, BSONElement>>& toUpsert() const {
+        return _toUpsert;
+    }
+
+    const std::vector<std::pair<ArrayIndexPath, BSONElement>>& toInsert() const {
+        return _toInsert;
+    }
+
+    std::string toStringDebug() const;
+
+    size_t computeApproxSize() const {
+        size_t sum = 0;
+        for (auto&& f : _toDelete) {
+            sum += f.approximateSizeInBytes();
+        }
+
+        for (auto&& [fr, elt] : _toUpsert) {
+            sum += fr.approximateSizeInBytes();
+            sum += elt.size();
+        }
+
+        for (auto&& [fr, elt] : _toInsert) {
+            sum += fr.approximateSizeInBytes();
+            sum += elt.size();
+        }
+        return sum;
+    }
+
+private:
+    static DocumentDiff computeDiffHelper(const BSONObj& pre,
+                                          const BSONObj& post,
+                                          const ArrayIndexPath& prefix);
+
+    std::vector<ArrayIndexPath> _toDelete;
+    std::vector<std::pair<ArrayIndexPath, BSONElement>> _toUpsert;
+    std::vector<std::pair<ArrayIndexPath, BSONElement>> _toInsert;
+};
+
+};  // namespace doc_diff
+
+};  // namespace mongo
